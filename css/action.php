fee43a4d303749ddb1fb6baee81f148f546a04b2
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-03-21 18:51:09 +0900

    Doc: layout-onepiece --> op-layout-onepiece

--- css/action.php
+++ css/action.php
@@ -1,10 +1,9 @@
 <?php
-/**
- * layout-onepiece:/css/action.php
+/** op-layout-onepiece:/css/action.php
  *
  * @creation  2017-09-07
  * @version   1.0
- * @package   layout-onepiece
+ * @package   op-layout-onepiece
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
-/**
- * layout-onepiece:/index.php
+/** op-layout-onepiece:/index.php
  *
  * @creation  2017-01-05
  * @version   1.0
- * @package   layout-onepiece
+ * @package   op-layout-onepiece
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
