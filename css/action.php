c15f1a9d5640f3edf56ae27a6baed8b9a17d28d0
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-03-24 10:30:37 +0900

    Fix: css > action.php | v2.0 - Automatically entry.

--- css/action.php
+++ css/action.php
@@ -1,17 +1,43 @@
 <?php
 /** op-layout-onepiece:/css/action.php
  *
- * @creation  2017-09-07
- * @version   1.0
- * @package   op-layout-onepiece
- * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
- * @copyright Tomoaki Nagahara All right reserved.
+ * @created    2017-09-07
+ * @updated    2023-03-24 2.0: Automatically entry
+ * @version    2.0
+ * @package    op-layout-onepiece
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
  */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
 //	...
-return [
-	__DIR__.'/reset',
-	__DIR__.'/color',
-	__DIR__.'/body',
-	__DIR__.'/header',
-	__DIR__.'/footer',
-];
+$return = [];
+
+//  ...
+$file_extention = '.css';
+
+//	...
+foreach( glob(__DIR__."/*{$file_extention}") as $file ){
+    //	...
+    if( $file[0] === '.' or $file[0] === '_' ){
+        continue;
+    }
+
+    //	...
+    $name = substr($file, 0, -(strlen($file_extention)));
+
+    //	...
+    $return[] = $name;
+}
+
+//	...
+return $return;
