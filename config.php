0003730845c344744bdb13e259a0eac0e71496a0
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-02-14 00:31:57 +0900

    New: config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/** op-layout-onepiece:/config.php
+ *
+ * @creation   2024-02-11
+ * @version    1.0
+ * @package    op-layout-onepiece
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+//	...
+return [
+	'darkmode' => 0,
+];
