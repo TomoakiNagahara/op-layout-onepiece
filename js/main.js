186465ffb645a8567e0a73a15548f4c879e651c3
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-02-13 23:53:25 +0900

    Doc: js > main.js | PHPDoc

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-/**
- * layout-onepiece:/js/main.js
+
+/** op-layout-onepiece:/js/main.js
  *
  * @creation  2019-03-13
  * @version   1.0
- * @package   layout-onepiece
+ * @package   op-layout-onepiece
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
