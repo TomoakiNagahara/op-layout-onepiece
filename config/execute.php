27441c54d8814342b0e8b9131246cf042005c9e0
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-02-13 23:52:59 +0900

    Doc: config > execute.php | PHPDoc

--- config/execute.php
+++ config/execute.php
@@ -1,9 +1,9 @@
 <?php
-/** app-skeleton-2020-nep:/asset/config/execute.php
+/** op-layout-onepiece:/asset/config/execute.php
  *
  * @created   2023-01-26
  * @version   1.0
- * @package   app-skeleton-2020-nep
+ * @package   op-layout-onepiece
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
