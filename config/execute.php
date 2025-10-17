3b25256d209f12494341c7f892f3ca45552cefbc
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-10-17 14:02:18 +0900

    Doc: config > execute.php | PHPDoc

--- config/execute.php
+++ config/execute.php
@@ -1,10 +1,10 @@
 <?php
-/** op-layout-onepiece:/asset/config/execute.php
+/** op-layout-onepiece:/config/execute.php
  *
  * @created   2023-01-26
  * @version   1.0
  * @package   op-layout-onepiece
- * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @author    Tomoaki Nagahara
  * @copyright Tomoaki Nagahara All right reserved.
  */
 
